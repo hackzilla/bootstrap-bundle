f22b72db23727bfda6fc2003055cfc6709d3c95d
Daniel Platt <github@ofdan.co.uk>
2014-04-19 13:43:47 +0100

    correct typo in classname

--- HackzillaBootstrapBundle.php
+++ HackzillaBootstrapBundle.php
@@ -4,7 +4,7 @@ namespace Hackzilla\Bundle\BootstrapBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class HackzillaBoostrapBundle extends Bundle
+class HackzillaBootstrapBundle extends Bundle
 {
     
 }
